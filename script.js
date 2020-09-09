0ff5941bac69c1e83f9db1461a48c92eb2b778d9
Jack Meier <jackson.meier423@gmail.com>
2020-09-08 21:29:33 -0700

    created h1 element

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Create your HTML Page via DOM Methods here!
-alert("test")
-let body = document;
-console.log(body);
+let body = document.body;
+let h1El = document.createElement("h1");
+h1El.textContent = "Hello, World";
+body.appendChild(h1El);
+h1El.style.textAlign = "center";
