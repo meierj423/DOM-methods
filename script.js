498a277e0e377a9a9de3e112354d97e0408321ac
Jack Meier <jackson.meier423@gmail.com>
2020-09-08 21:32:27 -0700

    created h2 element

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let body = document.body;
+
 let h1El = document.createElement("h1");
+let h2El = document.createElement("h2");
+
 h1El.textContent = "Hello, World";
+h2El.textContent = "Below h2";
+
 body.appendChild(h1El);
+body.appendChild(h2El);
+
 h1El.style.textAlign = "center";
+h2El.style.textAlign = "center";
